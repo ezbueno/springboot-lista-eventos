3345e6b7ee51f61b12f9496ac48bbd0bd864d43d
Ezandro Bueno <57026708+ezbueno@users.noreply.github.com>
2020-09-05 22:52:55 -0300

    Desenvolvimento de uma Lista de Eventos

---
+++ eventosapp/src/main/java/com/eventosapp/controllers/EventoController.java
@@ -0,0 +1,46 @@
+package com.eventosapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.eventosapp.models.Evento;
+import com.eventosapp.repository.EventoRepository;
+
+@Controller
+public class EventoController {
+	
+	@Autowired
+	private EventoRepository eventoRepository;
+	
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
+	public String form() {
+		return "evento/formEvento";
+	}
+	
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
+	public String form(Evento evento) {
+		eventoRepository.save(evento);
+		return "redirect:/cadastrarEvento";
+	}
+	
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = eventoRepository.findAll();
+		mv.addObject("eventos", eventos);
+		return mv; 
+	}
+	
+	@RequestMapping("/{codigo}")
+	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo) {
+		Evento evento = eventoRepository.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+		mv.addObject("evento", evento);
+		System.out.println("evento " + evento);
+		return mv;
+	}
+}
